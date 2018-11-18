0544d7d288cd5a07a2d40446afecb458f3977579
Pjott <piotr.baa@gmail.com>
2018-11-18 20:33:49 +0100

    start formy test

--- src/main/java/Formy/App.java
+++ src/main/java/Formy/App.java
@@ -1,5 +1,9 @@
 package Formy;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 /**
  * Hello world!
  *
@@ -8,6 +12,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver", "D:\\Programs\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://formy-project.herokuapp.com/form");
+
+        driver.findElement(By.id("first-name")).sendKeys("Jan");
+        driver.findElement(By.id("last-name")).sendKeys("Kowalski");
+        driver.findElement(By.id("job-title")).sendKeys("Kierowca");
+        driver.findElement(By.id("radio-button-2")).click();
+        driver.findElement(By.id("radio-button-2")).click();
+        driver.findElement(By.id("checkbox-1")).click();
+        driver.findElement(By.cssSelector("option[value='1']")).click();
+        driver.findElement(By.id("datepicker")).sendKeys("11/11/2020");
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+        driver.quit();
     }
 }
