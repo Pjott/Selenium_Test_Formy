f9374e8170096d62b9a2d15434b47f3d60c12ed6
Pjott <piotr.baa@gmail.com>
2018-11-19 20:43:28 +0100

    using Page object pattern in tests

--- src/main/java/Formy/App.java
+++ src/main/java/Formy/App.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.ConfirmationPage;
+import pages.FormPage;
+
 import static org.junit.Assert.assertEquals;
 
 public class App 
@@ -19,34 +22,14 @@ public class App
 
         driver.get("http://formy-project.herokuapp.com/form");
 
-        submitForm(driver);
+        FormPage formPage = new FormPage();
+        formPage.submitForm(driver);
 
-        waitForAlertBanner(driver);
+        ConfirmationPage confirmationPage = new ConfirmationPage();
+        confirmationPage.waitForAlertBanner(driver);
 
-        assertEquals("The form was successfully submitted!", getAlertBannerText(driver));
+        assertEquals("The form was successfully submitted!", confirmationPage.getAlertBannerText(driver));
 
         driver.quit();
-    }
-
-    public static void submitForm(WebDriver driver) {
-        driver.findElement(By.id("first-name")).sendKeys("Jan");
-        driver.findElement(By.id("last-name")).sendKeys("Kowalski");
-        driver.findElement(By.id("job-title")).sendKeys("Kierowca");
-        driver.findElement(By.id("radio-button-2")).click();
-        driver.findElement(By.id("radio-button-2")).click();
-        driver.findElement(By.id("checkbox-1")).click();
-        driver.findElement(By.cssSelector("option[value='1']")).click();
-        driver.findElement(By.id("datepicker")).sendKeys("11/11/2020");
-        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
-        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
-    }
-
-    public static void waitForAlertBanner(WebDriver driver) {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until((ExpectedConditions.visibilityOfElementLocated(By.className("alert"))));
-    }
-
-    public static String getAlertBannerText(WebDriver driver){
-        return driver.findElement(By.className("alert")).getText();
     }
 }
